bdc8b535703e3d3172d653ca64f8d824e28ab899
atsza661 <ats.altmets@gmail.com>
2023-03-26 22:41:58 +0300

    testing

--- weekendfile.py
+++ weekendfile.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import datetime
 
-# Read Saturday and Sunday data into separate dataframes
+#Read Saturday and Sunday data
 sat_df = pd.read_csv('data_2023-02-11.csv')
 sun_df = pd.read_csv('data_2023-02-12.csv')
 
-# Concatenate the dataframes vertically into one
+#Concatenate the dataframes
 combined_df = pd.concat([sat_df, sun_df], ignore_index=True)
 
 # Add a new column for the combined file generation date
 today = datetime.datetime.today().strftime('%Y-%m-%d')
-combined_df['Combined file generation date'] = today
+combined_df['combined_file_generation_date'] = today
 
-# Save the combined dataframe to a new CSV file
+# Save to a new CSV file
 combined_df.to_csv('weekend_data.csv', index=False)
